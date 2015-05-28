c5aca6bced61b8077c9ba60f6b9868fd231a4e58
Connor Peet <connor@peet.io>
2015-05-28 13:05:20 -1000

    Actually test things

--- tests/test.js
+++ tests/test.js
@@ -1,19 +1,35 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-function file(name) {
-    return fs.createWriteStream(__dirname + '/' + name);
+var assert = require('assert');
+var eq = require('buffer-equal');
+var qr = require('./../');
+
+function streamToBuffer(stream, cb) {
+    var bufs = [];
+
+    stream.on('data', function (data) {
+        bufs.push(data);
+    }).on('end', function () {
+        cb(Buffer.concat(bufs));
+    });
 }
 
-var qr = require('./../');
-var text = 'I \u2764\uFE0F QR code!';
+function compare(stream, file) {
+    streamToBuffer(stream, function (buf) {
+        assert(eq(buf, fs.readFileSync(__dirname + '/fixtures/' + file)));
+        console.log(file + ' ok!');
+    });
+}
+
 var text = 'https://yadi.sk/d/FuzPeEg-QyaZN?qr';
 var ec_level = 'Q';
 
-qr.image(text, { type: 'png', ec_level: ec_level, parse_url: false, margin: 1}).pipe(file('qr_f.png'));
-qr.image(text, { type: 'png', ec_level: ec_level, parse_url: true,  margin: 1}).pipe(file('qr_t.png'));
-qr.image(text, { type: 'svg', ec_level: ec_level}).pipe(file('qr.svg'));
-qr.image(text, { type: 'eps', ec_level: ec_level}).pipe(file('qr.eps'));
-qr.image(text, { type: 'pdf', ec_level: ec_level}).pipe(file('qr.pdf'));
+compare(qr.image(text, { type: 'png', ec_level: ec_level, parse_url: false, margin: 1}), 'qr_f.png');
+compare(qr.image(text, { type: 'png', ec_level: ec_level, parse_url: true,  margin: 1}), 'qr_t.png');
+compare(qr.image(text, { type: 'svg', ec_level: ec_level}), 'qr.svg');
+compare(qr.image(text, { type: 'eps', ec_level: ec_level}), 'qr.eps');
+compare(qr.image(text, { type: 'pdf', ec_level: ec_level}), 'qr.pdf');
 
-fs.writeFileSync('qr_sync.png', qr.imageSync(text));
+assert(eq(qr.imageSync(text), fs.readFileSync(__dirname + '/fixtures/qr_sync.png')));
+console.log('qr_sync.png ok!')
